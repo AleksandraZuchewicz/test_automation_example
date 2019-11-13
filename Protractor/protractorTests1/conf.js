34c36cf3688b5562061cfc20bcb89341e636b77c
Aleksandra Zuchewicz <aleksandra.zuchewicz@gmail.com>
2019-11-13 09:15:16 +0000

    added configuration option: run tests on firefox

--- Protractor/protractorTests1/conf.js
+++ Protractor/protractorTests1/conf.js
@@ -1,4 +1,8 @@
 exports.config = {
   seleniumAddress: "http://localhost:4444/wd/hub",
-  specs: ["spec2.js"]
+  specs: ["spec4.js"],
+  // if you dont provide any browser capabilities, it asumed that you will use chrome 
+  capabilities: {
+    'browserName': 'firefox'
+  }
 };
